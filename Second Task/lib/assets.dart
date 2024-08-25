0d8266bd58535e0b38c58918fc8da54c499abd68
zeyadali06 <z01551153743@gmail.com>
2024-08-25 22:14:48 +0300

    init files and folders

---
+++ Second Task/lib/assets.dart
@@ -0,0 +1,109 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsIconsDocFile
+  /// assets/icons/doc_file.svg
+  static const String assetsIconsDocFile = "assets/icons/doc_file.svg";
+
+  /// Assets for assetsIconsDocuments
+  /// assets/icons/Documents.svg
+  static const String assetsIconsDocuments = "assets/icons/Documents.svg";
+
+  /// Assets for assetsIconsDropBox
+  /// assets/icons/drop_box.svg
+  static const String assetsIconsDropBox = "assets/icons/drop_box.svg";
+
+  /// Assets for assetsIconsExcelFile
+  /// assets/icons/excel_file.svg
+  static const String assetsIconsExcelFile = "assets/icons/excel_file.svg";
+
+  /// Assets for assetsIconsFigmaFile
+  /// assets/icons/Figma_file.svg
+  static const String assetsIconsFigmaFile = "assets/icons/Figma_file.svg";
+
+  /// Assets for assetsIconsFolder
+  /// assets/icons/folder.svg
+  static const String assetsIconsFolder = "assets/icons/folder.svg";
+
+  /// Assets for assetsIconsGoogleDrive
+  /// assets/icons/google_drive.svg
+  static const String assetsIconsGoogleDrive = "assets/icons/google_drive.svg";
+
+  /// Assets for assetsIconsLogo
+  /// assets/icons/logo.svg
+  static const String assetsIconsLogo = "assets/icons/logo.svg";
+
+  /// Assets for assetsIconsMedia
+  /// assets/icons/media.svg
+  static const String assetsIconsMedia = "assets/icons/media.svg";
+
+  /// Assets for assetsIconsMediaFile
+  /// assets/icons/media_file.svg
+  static const String assetsIconsMediaFile = "assets/icons/media_file.svg";
+
+  /// Assets for assetsIconsMenuDashboard
+  /// assets/icons/menu_dashboard.svg
+  static const String assetsIconsMenuDashboard = "assets/icons/menu_dashboard.svg";
+
+  /// Assets for assetsIconsMenuDoc
+  /// assets/icons/menu_doc.svg
+  static const String assetsIconsMenuDoc = "assets/icons/menu_doc.svg";
+
+  /// Assets for assetsIconsMenuNotification
+  /// assets/icons/menu_notification.svg
+  static const String assetsIconsMenuNotification = "assets/icons/menu_notification.svg";
+
+  /// Assets for assetsIconsMenuProfile
+  /// assets/icons/menu_profile.svg
+  static const String assetsIconsMenuProfile = "assets/icons/menu_profile.svg";
+
+  /// Assets for assetsIconsMenuSetting
+  /// assets/icons/menu_setting.svg
+  static const String assetsIconsMenuSetting = "assets/icons/menu_setting.svg";
+
+  /// Assets for assetsIconsMenuStore
+  /// assets/icons/menu_store.svg
+  static const String assetsIconsMenuStore = "assets/icons/menu_store.svg";
+
+  /// Assets for assetsIconsMenuTask
+  /// assets/icons/menu_task.svg
+  static const String assetsIconsMenuTask = "assets/icons/menu_task.svg";
+
+  /// Assets for assetsIconsMenuTran
+  /// assets/icons/menu_tran.svg
+  static const String assetsIconsMenuTran = "assets/icons/menu_tran.svg";
+
+  /// Assets for assetsIconsOneDrive
+  /// assets/icons/one_drive.svg
+  static const String assetsIconsOneDrive = "assets/icons/one_drive.svg";
+
+  /// Assets for assetsIconsPdfFile
+  /// assets/icons/pdf_file.svg
+  static const String assetsIconsPdfFile = "assets/icons/pdf_file.svg";
+
+  /// Assets for assetsIconsSearch
+  /// assets/icons/Search.svg
+  static const String assetsIconsSearch = "assets/icons/Search.svg";
+
+  /// Assets for assetsIconsSoundFile
+  /// assets/icons/sound_file.svg
+  static const String assetsIconsSoundFile = "assets/icons/sound_file.svg";
+
+  /// Assets for assetsIconsUnknown
+  /// assets/icons/unknown.svg
+  static const String assetsIconsUnknown = "assets/icons/unknown.svg";
+
+  /// Assets for assetsIconsXdFile
+  /// assets/icons/xd_file.svg
+  static const String assetsIconsXdFile = "assets/icons/xd_file.svg";
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.png
+  static const String assetsImagesLogo = "assets/images/logo.png";
+
+  /// Assets for assetsImagesProfilePic
+  /// assets/images/profile_pic.png
+  static const String assetsImagesProfilePic = "assets/images/profile_pic.png";
+}
+
