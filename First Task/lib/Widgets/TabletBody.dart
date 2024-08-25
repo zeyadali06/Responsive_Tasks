c33b2e83952de40b3b6365951d22e9f959fde6f5
zeyadali06 <z01551153743@gmail.com>
2024-08-25 18:15:42 +0300

    finish task

--- First Task/lib/Widgets/TabletBody.dart
+++ First Task/lib/Widgets/TabletBody.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_designs/Widgets/CustomCalendar.dart';
 import 'package:responsive_designs/Widgets/TopBar.dart';
 import 'package:responsive_designs/Widgets/SalesOverview.dart';
 import 'package:responsive_designs/Widgets/TabletCustomerSection.dart';
+import 'package:responsive_designs/Widgets/CustomCalendar.dart';
 import 'package:responsive_designs/Widgets/UpcomingScheduleListView.dart';
 import 'package:responsive_designs/Widgets/UpcomingScheduleStatistics.dart';
 
@@ -26,45 +26,41 @@ class TabletBody extends StatelessWidget {
           const SliverToBoxAdapter(child: TopBar()),
           const SliverToBoxAdapter(child: SizedBox(height: 20)),
           SliverToBoxAdapter(child: TabletCustomerSection(width: width)),
-          SliverList(
-            delegate: SliverChildListDelegate.fixed(
-              [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(width: 20),
-                    const Expanded(
-                      flex: 3,
-                      child: Column(
-                        children: [
-                          AspectRatio(
-                            aspectRatio: 2 / 1,
-                            child: SalesOverview(
-                              values: [7700, 6500, 4200, 5000, 7400, 9200, 6300, 7400, 3800, 5000, 7200, 8500],
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                          UpcomingScheduleStatistics(),
-                        ],
+          SliverToBoxAdapter(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(width: 20),
+                const Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 2 / 1,
+                        child: SalesOverview(
+                          values: [7700, 6500, 4200, 5000, 7400, 9200, 6300, 7400, 3800, 5000, 7200, 8500],
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 15),
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        children: [
-                          CustomCalendar(onDaySelected: (DateTime date) {}),
-                          const SizedBox(height: 10),
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: UpcomingScheduleListView(),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(width: 20),
-                  ],
+                      SizedBox(height: 15),
+                      UpcomingScheduleStatistics(),
+                    ],
+                  ),
                 ),
+                const SizedBox(width: 15),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      CustomCalendar(onDaySelected: (DateTime date) {}),
+                      const SizedBox(height: 10),
+                      AspectRatio(
+                        aspectRatio: 1,
+                        child: UpcomingScheduleListView(),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 20),
               ],
             ),
           ),
